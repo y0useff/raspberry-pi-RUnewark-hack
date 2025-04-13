a514bedc6a8c921e509b1b7f13457e8e88b5875c
y0useff <therealyousefghaly@gmail.com>
2025-04-13 15:06:32 -0400

    message

--- newark.py
+++ newark.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import time
+
 BASE_URL = "https://www.musiciansfriend.com"
 # category = input()
 number_of_pages = 3
-
-SALE_PARAMETERS = "#N=100202+100203+500257+100202+100203&pageName=department-page"
-SALE = input("Sale? Enter Y or N.")
-
+best_deal = {}
+best_deal["discount"] = 0
 def get_html_content(url):
     try:
         response = requests.get(url)
@@ -17,55 +18,91 @@ def get_html_content(url):
         return None
 #make request getting html body, pass into beautiful soup
 
+products = []
 #iterate through every product in a page
-for i in range(0, number_of_pages):
-    nao = i * 30
-    if SALE.lower() == "y":
-        category_url = f"{BASE_URL}/bass?Nao={nao}&{SALE_PARAMETERS}"
-    else:
+def scrape():
+    for i in range(0, number_of_pages):
+        nao = i * 30
         category_url = f"{BASE_URL}/bass?Nao={nao}"
-    print(category_url)
-    html_text = get_html_content(category_url)
-    soup = BeautifulSoup(html_text)
+        print(category_url)
+        html_text = get_html_content(category_url)
+        soup = BeautifulSoup(html_text)
 
-    #get list of products
-    product_card_list = soup.find_all('div', {"class", 'product-card'})
+        #get list of products
+        product_card_list = soup.find_all('div', {"class", 'product-card'})
 
-    for element in product_card_list:
-        #grab product card
-        card = (element.select(".product-card-content"))[0]
+        for element in product_card_list:
+            #grab product card
+            card = (element.select(".product-card-content"))[0]
 
-        #get text from link
-        link = (card.select(".ui-link"))[0]
-        product_name = link.get_text()
-        
-        price_list = card.select('.sale-price')
-        price = 0
-        discount = 0
-        #if no savings, or multiple prices, price is set to the only price
-        if len(price_list) == 1:
-            price = price_list[0].get_text()
-            price = price.split("$")[1]
-            price = price.replace(",", "")
-        else:
-            #if multiple prices, discount found
-            p = []
-            for tag in price_list:
-                dollar_price = tag.get_text().split('$', 1)[1]
-                dollar_price = dollar_price.split("\n")[0]
-                dollar_price = dollar_price.replace(",", "")
-                p.append(dollar_price)
-            p = list(map(float, p))
-            if len(p) != 0: 
-                price = min(p)
-                discount = max(p) - min(p) 
-
-        print("*******************************")
-        print(f"Page: {i}")
-        print(f"Name:{product_name}")
-        print(f"Price: \n{price}")
-        print(f"Discount:\n{discount}")
-        print("*******************************")
-
+            #get text from link
+            link = (card.select(".ui-link"))[0]
+            product_name = link.get_text().replace("\n", "")
             
-        
+            price_list = card.select('.sale-price')
+            price = 0
+            discount = 0
+            #if no savings, or multiple prices, price is set to the only price
+            if len(price_list) == 1:
+                price = price_list[0].get_text()
+                price = price.split("$")[1]
+                price = price.replace(",", "")
+            else:
+                #if multiple prices, discount found
+                p = []
+                for tag in price_list:
+                    dollar_price = tag.get_text().split('$', 1)[1]
+                    dollar_price = dollar_price.split("\n")[0]
+                    dollar_price = dollar_price.replace(",", "")
+                    p.append(dollar_price)
+                p = list(map(float, p))
+                if len(p) != 0: 
+                    price = round(min(p), 2)
+                    discount = round(max(p) - min(p), 2)
+                    if discount > best_deal["discount"]:
+                        best_deal["page"] = i
+                        best_deal["name"] = product_name
+                        best_deal["price"] = price
+                        best_deal["discount"] = discount
+                        
+                
+                product_dict = {
+                    "page": i,
+                    "name": product_name,
+                    "price": price, 
+                    "discount": discount, 
+                }
+            
+                products.append(product_dict)
+
+            print("*******************************")
+            print(f"Page: {i}")
+            print(f"Name:{product_name}")
+            print(f"Price: \n${price}")
+            print(f"Discount:\n${discount}")
+        with open("data.json", "w") as file:
+            json.dump(products, file, indent=4)
+        print("*******************************")
+
+    print(best_deal)
+
+    url = "https://api.pushover.net/1/messages.json"
+
+    data = {
+        "token": "akcbutkgdd49tab1kwzjtww3rxispq",
+        "user": "uqmfguy4jn59b2vauxjwkqru152jv1",
+        "message": f'Scraping all done! We found a great deal for you. Save ${best_deal["discount"]:.2f} on a "{best_deal["name"]}"!'
+    }
+
+    response = requests.post(url, data=data)
+
+    # Optional: print the response status and body
+    print(f"Status Code: {response.status_code}")
+    print(f"Response: {response.text}")     
+
+def main():
+    while True:
+        scrape()
+        time.sleep(60)  # Wait 60 seconds before the next scrape
+
+main()
