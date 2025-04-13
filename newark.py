8541ecc8b0a24f14ac951fbb2cc189a774b081d5
y0useff <therealyousefghaly@gmail.com>
2025-04-13 15:38:00 -0400

    yeah :

--- newark.py
+++ newark.py
@@ -4,7 +4,20 @@ import json
 import time
 
 BASE_URL = "https://www.musiciansfriend.com"
-# category = input()
+
+print("Select a category: ")
+print("Electric Guitars")
+print("Bass")
+print("Amplifiers Effects")
+print("Drums Percussion")
+print("Keyboards MIDI")
+print("Live Sound")
+print("Recording Gear")
+print("Accessories")
+print("New Arrivals")
+
+category = input().lower().replace(" ", "-")
+
 number_of_pages = 3
 best_deal = {}
 best_deal["discount"] = 0
@@ -23,7 +36,7 @@ products = []
 def scrape():
     for i in range(0, number_of_pages):
         nao = i * 30
-        category_url = f"{BASE_URL}/bass?Nao={nao}"
+        category_url = f"{BASE_URL}/{category}?Nao={nao}"
         print(category_url)
         html_text = get_html_content(category_url)
         soup = BeautifulSoup(html_text)
@@ -59,7 +72,7 @@ def scrape():
                 if len(p) != 0: 
                     price = round(min(p), 2)
                     discount = round(max(p) - min(p), 2)
-                    if discount > best_deal["discount"]:
+                    if discount >= best_deal["discount"]:
                         best_deal["page"] = i
                         best_deal["name"] = product_name
                         best_deal["price"] = price
@@ -87,18 +100,31 @@ def scrape():
     print(best_deal)
 
     url = "https://api.pushover.net/1/messages.json"
+    if best_deal["discount"] != 0:
+        data = {
+            "token": "akcbutkgdd49tab1kwzjtww3rxispq",
+            "user": "uqmfguy4jn59b2vauxjwkqru152jv1",
+            "message": f'Scraping all done! We found a great deal for you. Save ${best_deal["discount"]:.2f} on a "{best_deal["name"]}"!'
+        }
+        response = requests.post(url, data=data)
 
-    data = {
-        "token": "akcbutkgdd49tab1kwzjtww3rxispq",
-        "user": "uqmfguy4jn59b2vauxjwkqru152jv1",
-        "message": f'Scraping all done! We found a great deal for you. Save ${best_deal["discount"]:.2f} on a "{best_deal["name"]}"!'
-    }
+        # Optional: print the response status and body
+        print(f"Status Code: {response.status_code}")
+        print(f"Response: {response.text}")
+    else:
+        data = {
+            "token": "akcbutkgdd49tab1kwzjtww3rxispq",
+            "user": "uqmfguy4jn59b2vauxjwkqru152jv1",
+            "message": f'Scraping all done! We found some great deals for you!"!'
+        }
+        response = requests.post(url, data=data)
 
-    response = requests.post(url, data=data)
+        # Optional: print the response status and body
+        print(f"Status Code: {response.status_code}")
+        print(f"Response: {response.text}")
 
-    # Optional: print the response status and body
-    print(f"Status Code: {response.status_code}")
-    print(f"Response: {response.text}")     
+
+
 
 def main():
     while True:
